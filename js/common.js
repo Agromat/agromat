945a0a2b3b013a83de02fa1201b60b8d54950908
Yevgen <godun.yevgen@gmail.com>
2012-11-13 16:27:25 +0200

    index

--- js/common.js
+++ js/common.js
@@ -3,6 +3,40 @@ $(document).ready(function() {
         fx:     'fade',
         timeout: 2000,
         speed: 2000,
-        pager:  '.slider-nav'
+        pager:  '.js-main-nav'
     });
+    $('.js-slider-comp').cycle({ 
+        fx:     'fade',
+        timeout: 2000,
+        speed: 2000,
+        pager:  '.js-our-comp-nav'
+    });
+    $('.js-scroll-shop').scrollable({
+        next:'.next_shop',
+        prev:'.prev_shop'
+    });
+     if ($(".js-scroll-shop").length>0) {
+       // Get the Scrollable control
+       var scrollable_list_1 = $(".js-scroll-shop").data("scrollable");
+       // Set to the number of visible items
+       var number_list = 5;
+       // Handle the Scrollable control's onSeek event
+       scrollable_list_1.onSeek(function(event, index) {
+         // Check to see if we're at the end
+         if (this.getIndex() >= this.getSize() - number_list) {      // Disable the Next link
+           $(".next_shop").addClass("disabled");
+         }
+       });
+       // Handle the Scrollable control's onBeforeSeek event
+       scrollable_list_1.onBeforeSeek(function(event, index) {
+         // Check to see if we're at the end
+         if (this.getIndex() >= this.getSize() - number_list) { 
+           // Check to see if we're trying to move forward
+           if (index > this.getIndex()) {
+             // Cancel navigation
+             return false;
+           }
+         }
+       });
+     }
 });
