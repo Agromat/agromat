9f179c46cb872f2fd37310eae516b371df559f0f
Yevgen <godun.yevgen@gmail.com>
2012-11-19 16:22:19 +0200

    new pages

--- js/common.js
+++ js/common.js
@@ -1,42 +1,54 @@
 $(document).ready(function() {
-    $('.js-slider').cycle({ 
-        fx:     'fade',
-        timeout: 2000,
-        speed: 1000,
-        pager:  '.js-main-nav'
-    });
-    $('.js-slider-comp').cycle({ 
-        fx:     'fade',
-        timeout: 2000,
-        speed: 1000,
-        pager:  '.js-our-comp-nav'
-    });
-    $('.js-scroll-shop').scrollable({
-        next:'.next_shop',
-        prev:'.prev_shop'
-    });
-     if ($(".js-scroll-shop").length>0) {
-       // Get the Scrollable control
-       var scrollable_list_1 = $(".js-scroll-shop").data("scrollable");
-       // Set to the number of visible items
-       var number_list = 5;
-       // Handle the Scrollable control's onSeek event
-       scrollable_list_1.onSeek(function(event, index) {
-         // Check to see if we're at the end
-         if (this.getIndex() >= this.getSize() - number_list) {      // Disable the Next link
-           $(".next_shop").addClass("disabled");
+  $('.js-slider').cycle({ 
+      fx:     'fade',
+      timeout: 2000,
+      speed: 1000,
+      pager:  '.js-main-nav'
+  });
+  $('.js-slider-comp').cycle({ 
+      fx:     'fade',
+      timeout: 2000,
+      speed: 1000,
+      pager:  '.js-our-comp-nav'
+  });
+  $('.js-scroll-shop').scrollable({
+      next:'.next_shop',
+      prev:'.prev_shop'
+  });
+  $('.js-slider-cont').cycle({ 
+      fx:     'fade',
+      timeout: 2000,
+      speed: 1000,
+      pager:  '.js-main-nav'
+  });
+   if ($(".js-scroll-shop").length>0) {
+     // Get the Scrollable control
+     var scrollable_list_1 = $(".js-scroll-shop").data("scrollable");
+     // Set to the number of visible items
+     var number_list = 5;
+     // Handle the Scrollable control's onSeek event
+     scrollable_list_1.onSeek(function(event, index) {
+       // Check to see if we're at the end
+       if (this.getIndex() >= this.getSize() - number_list) {      // Disable the Next link
+         $(".next_shop").addClass("disabled");
+       }
+     });
+     // Handle the Scrollable control's onBeforeSeek event
+     scrollable_list_1.onBeforeSeek(function(event, index) {
+       // Check to see if we're at the end
+       if (this.getIndex() >= this.getSize() - number_list) { 
+         // Check to see if we're trying to move forward
+         if (index > this.getIndex()) {
+           // Cancel navigation
+           return false;
          }
-       });
-       // Handle the Scrollable control's onBeforeSeek event
-       scrollable_list_1.onBeforeSeek(function(event, index) {
-         // Check to see if we're at the end
-         if (this.getIndex() >= this.getSize() - number_list) { 
-           // Check to see if we're trying to move forward
-           if (index > this.getIndex()) {
-             // Cancel navigation
-             return false;
-           }
-         }
-       });
-     }
+       }
+     });
+   }
+   $(".video__play").click(function(){
+      $(this).prev().hide();
+      $(this).next().show();
+      $(this).hide();
+   });
+
 });
